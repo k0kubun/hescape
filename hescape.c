6cf7d2e060bfe53f6cfa4cb126739d8ff944478e
Takashi Kokubun <takashikkbn@gmail.com>
2016-08-11 23:44:02 +0900

    Utilize pcmpestri intrinsics

--- hescape.c
+++ hescape.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
 #include "hescape.h"
+
+#ifdef __SSE4_2__
+# ifdef _MSC_VER
+#  include <nmmintrin.h>
+# else
+#  include <x86intrin.h>
+# endif
+#endif
+
+#if __GNUC__ >= 3
+# define likely(x) __builtin_expect(!!(x), 1)
+# define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+# define likely(x) (x)
+# define unlikely(x) (x)
+#endif
 
 // Non-null and zero terminated buffer.
 char __hesc_initbuf[1];
@@ -40,6 +57,29 @@ static const char HTML_ESCAPE_TABLE[] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
 
+static const char*
+find_escaped_fast(const char *buf, const char *buf_end, int *found)
+{
+  static const char ranges[] = "\"&'<>";
+
+  if (likely(buf_end - buf >= 16)) {
+    __m128i ranges16 = _mm_loadu_si128((const __m128i *)ranges);
+    size_t left = (buf_end - buf) & ~15;
+    do {
+      __m128i b16 = _mm_loadu_si128((void *)buf);
+      int index = _mm_cmpestri(ranges16, 5, b16, 16, _SIDD_CMP_EQUAL_ANY);
+      if (unlikely(index != 16)) {
+        buf += index;
+        *found = 1;
+        break;
+      }
+      buf += 16;
+      left -= 16;
+    } while(likely(left != 0));
+  }
+  return buf;
+}
+
 /*
  * Replace characters according to the following rules.
  * This function can handle only ASCII-compatible string.
@@ -53,18 +93,30 @@ static const char HTML_ESCAPE_TABLE[] = {
  * @return 1 if there is escaped characters, which means that dest->str is newly allocated.
  */
 int
-hesc_escape_html(hesc_buf *dest, const uint8_t *src, size_t len)
+hesc_escape_html(hesc_buf *dest, const uint8_t *buf, size_t len)
 {
-  const uint8_t *head = src, *end = src + len;
-  int ret = 0;
+  const uint8_t *head = buf, *buf_end = buf + len;
+  int ret = 0, found;
 
-  while (src < end) {
-    while (src < end && HTML_ESCAPE_TABLE[*src] == 0)
-      src++;
-    if (HTML_ESCAPE_TABLE[*src]) {
+  while (buf < buf_end) {
+    buf = find_escaped_fast(buf, buf_end, &found);
+    if (found) {
+      if (HTML_ESCAPE_TABLE[*buf]) {
+        ret = 1;
+      }
+      buf++;
+    } else {
+      break;
+    }
+  }
+
+  while (buf < buf_end) {
+    while (buf < buf_end && HTML_ESCAPE_TABLE[*buf] == 0)
+      buf++;
+    if (HTML_ESCAPE_TABLE[*buf]) {
       ret = 1;
     }
-    src++;
+    buf++;
   }
 
   dest->str = (char *)head;
--- test/run.c
+++ test/run.c
@@ -5,7 +5,7 @@
 int
 main(void)
 {
-  const char *src = "1234567890123456<";
+  const char *src = "1234567890<234567890";
   hesc_buf buf = HESC_BUF_INIT;
   hesc_escape_html(&buf, src, strlen(src));
   printf("result: '%s'\n", buf.str);
